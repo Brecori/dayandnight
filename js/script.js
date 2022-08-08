a772bb9b11e44828fbff3bae9f9943d42b94540a
Gustavo Omai <omai.gustavoh@gmail.com>
2022-08-08 19:24:42 -0300

    Começando o switch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-$('#dn').change(function(){
-    if($('#dn').is(":checked")) {
-        $('#header').addClass("header-white");
-        $('#header').removeClass("header-black");
-    }
-    
-    if($('#dn').not(":checked")) {
-        $('#header').addClass("header-black");
-        $('#header').removeClass("header-white");
+$("#dn").change(function() {
+    if(this.checked) {//Noite
+        $("#header").addClass("header-black");
+        $("#header").removeClass("header-white");
+        $("li, a").addClass("white-font");
+        $("li, a").removeClass("black-font");
+    }else{//Dia
+        $("#header").addClass("header-white");
+        $("#header").removeClass("header-black");
+        $("li, a").addClass("black-font");
+        $("li, a").removeClass("white-font");
     }
 });
