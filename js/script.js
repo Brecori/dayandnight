9a9ffaee749c090f83cad23b02743cdb2133244c
Gustavo Omai <omai.gustavoh@gmail.com>
2022-08-08 20:41:53 -0300

    merging

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ $("#dn").change(function() {
         $("#header").removeClass("header-white");
         $("li, a").addClass("white-font");
         $("li, a").removeClass("black-font");
+        $(".img-circleblack").removeClass(".img-circleblack");
+        $(".img-circleblack").addClass(".img-circlewhite");
+
     }else{//Dia
         $("#header").addClass("header-white");
         $("#header").removeClass("header-black");
         $("li, a").addClass("black-font");
         $("li, a").removeClass("white-font");
+        $(".img-circleblack").removeClass(".img-circlewhite");
+        $(".img-circleblack").addClass(".img-circleblack");
     }
 });
