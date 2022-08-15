15d24bd8e8ea3fc0cb3151c9d77827748b12124f
Breno Tosi <breno_tosi@hotmail.com>
2022-08-15 16:36:24 -0300

    Filter OK

--- js/script.js
+++ js/script.js
@@ -89,3 +89,16 @@ window.onscroll = function() {
             
         })
     });
+
+$(document).ready(function(){
+    var $btns = $('.btn').click(function() {
+        if (this.id == 'all') {
+          $('#parent > div').fadeIn(450);
+        } else {
+          var $el = $('.' + this.id).fadeIn(450);
+          $('#parent > div').not($el).hide();
+        }
+        $btns.removeClass('active-btn');
+        $(this).addClass('active-btn');
+      })
+})
